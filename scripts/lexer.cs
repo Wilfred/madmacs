f3284ecd8b2bacb4eff39df33064b49f4ada1d32
Wilfred Hughes <me@wilfred.me.uk>
2013-02-02 00:50:51 +0000

    lexing numbers

--- scripts/lexer.cs
+++ scripts/lexer.cs
@@ -1,9 +1,22 @@
 define((require) ->
   lex = (string) ->
+    tokens = []
+    
     comments = /;.*(\n|$)/g
     string = string.replace(comments, "")
 
-    string
+    numbers = /[0-9]+/
+    while true
+      match = numbers.exec(string)
+
+      if match == null
+        break
+      else
+        token = match[0]
+        tokens.push(token)
+        string = string.slice(token.length)
+
+    tokens
 
   return {"lex": lex}
 )
