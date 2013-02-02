139debde3f035618190628849f218873f82fb940
Wilfred Hughes <me@wilfred.me.uk>
2013-02-02 00:20:01 +0000

    write to the minibuffer when eval is clicked

--- scripts/main.cs
+++ scripts/main.cs
@@ -3,6 +3,9 @@ define((require) ->
   writeToMinibuffer = (message) ->
     minibuffer = document.getElementById "minibuffer"
     minibuffer.textContent = message
-    
-  writeToMinibuffer "hello world!"
+
+  evalButton = document.getElementById "eval"
+  evalButton.addEventListener('click', () ->
+    writeToMinibuffer "hello again world"
+  false)
 )
