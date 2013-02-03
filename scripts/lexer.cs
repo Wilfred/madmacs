e0b6634b09d119dbd32144b89ba9a346ec471247
Wilfred Hughes <me@wilfred.me.uk>
2013-02-03 16:09:25 +0000

    fixing a bug where "1;\n2" was parsed as "12"

--- scripts/lexer.cs
+++ scripts/lexer.cs
@@ -2,14 +2,13 @@ define((require) ->
   lex = (string) ->
     tokens = []
     
-    comments = /;.*(\n|$)/g
-    string = string.replace(comments, "")
-
+    comments = /^;.*(\n|$)/
     numbers = /^[0-9]+/
     whitespace = /^\s+/
     while string.length
       numberMatch = numbers.exec(string)
       whitespaceMatch = whitespace.exec(string)
+      commentsMatch = comments.exec(string)
 
       if numberMatch != null
         token = numberMatch[0]
@@ -18,6 +17,10 @@ define((require) ->
       else if whitespaceMatch != null
         # throw it away
         token = whitespaceMatch[0]
+        string = string.slice(token.length)
+      else if commentsMatch != null
+        # throw it away
+        token = commentsMatch[0]
         string = string.slice(token.length)
       else
         # todo: error if there's unlexed text
