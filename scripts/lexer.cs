3065c617013ba7d856b56f0eb7b958b00428f920
Wilfred Hughes <me@wilfred.me.uk>
2013-02-03 16:29:43 +0000

    lex symbols and brackets

--- scripts/lexer.cs
+++ scripts/lexer.cs
@@ -1,17 +1,37 @@
 define((require) ->
   lex = (string) ->
     tokens = []
-    
+
+    # the tokens we recognise
+    symbol = /^[a-zA-Z*/+-]+/
     comments = /^;.*(\n|$)/
     numbers = /^[0-9]+/
+    openParen = /^\(/
+    closeParen = /^\)/
     whitespace = /^\s+/
+    
     while string.length
+      symbolMatch = symbol.exec(string)
       numberMatch = numbers.exec(string)
+      openParenMatch = openParen.exec(string)
+      closeParenMatch = closeParen.exec(string)
       whitespaceMatch = whitespace.exec(string)
       commentsMatch = comments.exec(string)
 
-      if numberMatch != null
+      if symbolMatch != null
+        token = symbolMatch[0]
+        tokens.push(token)
+        string = string.slice(token.length)
+      else if numberMatch != null
         token = numberMatch[0]
+        tokens.push(token)
+        string = string.slice(token.length)
+      else if openParenMatch != null
+        token = openParenMatch[0]
+        tokens.push(token)
+        string = string.slice(token.length)
+      else if closeParenMatch != null
+        token = closeParenMatch[0]
         tokens.push(token)
         string = string.slice(token.length)
       else if whitespaceMatch != null
